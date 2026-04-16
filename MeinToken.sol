91293c4dcfae929959931f67d5ee13270cc266b8
Gingi <iaw.internetagentur@gmail.com>
2026-04-16 14:59:48 +0200

    Erster Base Token Contract

--- MeinToken.sol
+++ MeinToken.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts@5.0.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@5.0.0/token/ERC20/extensions/ERC20Burnable.sol";
+
+// Wir haben "Ownable" und die "mint"-Funktion entfernt
+contract LuisliFixed is ERC20, ERC20Burnable {
+    
+    constructor() ERC20("Luisli Gold", "LGOLD") {
+        // Hier werden 21 Millionen Token erstellt (wie bei Bitcoin)
+        // Danach kann NIEMAND mehr neue erstellen.
+        _mint(msg.sender, 21000000 * 10**decimals());
+    }
+}
