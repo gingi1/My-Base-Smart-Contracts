0a6de6fb37fb915632e6ff8462986430ab828019
Gingi <iaw.internetagentur@gmail.com>
2026-04-20 14:48:58 +0200

    Update LuisliFixed contract for token creation
    Removed 'Ownable' and 'mint' function. Created token for Base Guild Level.

--- MeinToken.sol
+++ MeinToken.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts@5.0.0/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@5.0.0/token/ERC20/extensions/ERC20Burnable.sol";
 
 // Wir haben "Ownable" und die "mint"-Funktion entfernt
+// Dieser Token wurde für das Base Guild Level erstellt.
 contract LuisliFixed is ERC20, ERC20Burnable {
     
     constructor() ERC20("Luisli Gold", "LGOLD") {
