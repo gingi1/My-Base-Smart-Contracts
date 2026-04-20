8850013ac0fa32dd45900c3ddb2a51876f952ebd
Gingi <iaw.internetagentur@gmail.com>
2026-04-20 14:51:58 +0200

    Create LuisliGold ERC20 token contract
    Added ERC20 token contract for Luisli Gold with minting.

--- LuisliGold.sol
+++ LuisliGold.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract LuisliGold is ERC20 {
+    /**
+     * @dev Der Constructor setzt den Namen, das Symbol und den gesamten Supply.
+     * 1.000.000 Token mit 18 Dezimalstellen.
+     */
+    constructor() ERC20("Luisli Gold", "LGOLD") {
+        _mint(msg.sender, 1000000 * 10**decimals());
+    }
+}
